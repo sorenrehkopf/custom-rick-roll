a7fe1077d38391aa150d1dca5de7cac0acbcb831
Soren Smith <Soren.Smith@indigoslate.com>
2017-02-28 11:20:35 -0800

    showing response message and hiding form

--- src/scripts/components/main.component.jsx
+++ src/scripts/components/main.component.jsx
@@ -4,12 +4,25 @@ import SendForm from './sendForm.component.jsx';
 
 class Main extends Component {
 
+	constructor(){
+		super();
+		this.showForm = true;
+	}
+
+	responseHandler(data){
+		console.log(data);
+		this.responseUrl = data;
+		this.showForm = false;
+		this.forceUpdate();
+	}
+
 	render(){
 		return (
 				<div className="app-container">
 					<h1>Custom rick roll generator</h1>
 					<p>Use the form below to get a custom link to a fake webpage that will actually just auto play Rick Astley&#39;s iconic hit <em>Never Gonna Give You Up</em> for anyone who visits.</p>
-					<SendForm/>
+					<SendForm responseHandler={this.responseHandler.bind(this)} showMe={this.showForm}/>
+					<a href={this.responseUrl} target="_blank"><h1>{this.responseUrl}</h1></a>
 				</div>
 			)
 	}
--- src/scripts/components/sendForm.component.jsx
+++ src/scripts/components/sendForm.component.jsx
@@ -5,30 +5,35 @@ import FormService from '../services/formService.jsx'
 
 class SendForm extends Component {
 
+	constructor(props){
+		super(props);
+		this.submitForm = this.submitForm.bind(this);
+	}
+
 	submitForm(e){
 		e.preventDefault();
 		var data = FormService.getJSON(e.target);
 		console.log(data);
 		PageService.createPage(data)
-		.then(data=>{
-			console.log(data);
-		}).catch(err=>{
-			console.log(err);
-		})
+		.then(this.props.responseHandler);
 	}
 
 	render(){
-		return(
+		var el = null;
+		if(this.props.showMe){
+			el = (
 				<form onSubmit={this.submitForm} className="form pure-form">
 					<fieldset className="pure-group">
-						<input className="pure-input-1" type="text" placeholder="Page Title" name="title" max="50"></input>
-						<input className="pure-input-1" type="text" placeholder="Preview Image" name="image" max="500"></input>
+						<input className="pure-input-1" type="text" placeholder="Page Title" name="title" max="50" required="true"></input>
+						<input className="pure-input-1" type="text" placeholder="Preview Image" name="image" max="500"  required="true"></input>
 						<textarea className="pure-input-1" type="text" placeholder="Custom message (optional)" name="message" max="250" rows="3"></textarea>
-						<textarea className="pure-input-1" type="text" placeholder="Page description" name="description" max="150" rows="5"></textarea>
+						<textarea className="pure-input-1" type="text" placeholder="Page description" name="description" max="150" rows="5" required="true"></textarea>
 					</fieldset>
 					<button className="pure-button pure-input-1 pure-button-primary" type="submit">Make it!</button>
 				</form>
-			);
+			);	
+		}
+		return el;
 	}
 
 }
