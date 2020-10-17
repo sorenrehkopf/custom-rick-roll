099ad1b342bdf7578f47a1eff3d3abd68acad29f
sorenrehkopf <sorenrehkopfsmith@gmail.com>
2020-10-17 14:27:08 -0700

    diversify source domains

--- src/scripts/services/formService.jsx
+++ src/scripts/services/formService.jsx
@@ -1,11 +1,20 @@
+var sourceOptionsMap = {
+	blog: ['https://www.thisworldthesedays.com', 'https://www.tomorrowtides.com'],
+	paper: ['https://www.theraleighregister.com', 'https://www.sanfransentinel.com']
+}
+
 class FormService {
 
 	static getJSON(el){
 		var inputs = el.querySelectorAll('[name]');
-		console.log(inputs);
 		var data = {};
-		for(var i = 0;i < inputs.length ;i++){
-			data[inputs[i].getAttribute('name')] = inputs[i].value;
+
+		for(var i = 0;i < inputs.length ;i++) {
+			if (inputs[i].getAttribute('name') === 'source') {
+				data[inputs[i].getAttribute('name')] = sourceOptionsMap[inputs[i].value][Math.round(Math.random())];
+			} else {
+				data[inputs[i].getAttribute('name')] = inputs[i].value;
+			}
 		};
 		return data;
 	};
